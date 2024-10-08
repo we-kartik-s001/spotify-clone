abc6219edf647a686d691b374af8102a28b7a993
Kartik Shah <kartik-s001@webreinvent.com>
2024-10-08 11:38:34 +0530

    update: (package-installation) installed pinia and vue-router and setup pinia for use in application

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia';
 import App from './App.vue'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
-createApp(App).mount('#app')
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+const app = createApp(App);
+app.use(pinia);
+app.mount('#app');
